ef297d5de67275c69b2c7b3faaccb0857f7cbcf9
SamsonsAutomation <SamsonsAutomation@proton.me>
2026-09-20 15:07:37 -0400

    fixed some stuff I messed up

--- config.js
+++
@@ -1,7 +0,0 @@
-// Public frontend configuration.
-// API_BASE_URL is the URL Cloudflare gives you after deploying the Worker.
-// Example: https://samsons-timeclock-api.your-subdomain.workers.dev
-window.SAMSONS_CLOCK_CONFIG = {
-  API_BASE_URL: "https://samsons-timeclock-api.samsons-worker.workers.dev/",
-  COMPANY_SLUG: "samsons-demo"
-};
